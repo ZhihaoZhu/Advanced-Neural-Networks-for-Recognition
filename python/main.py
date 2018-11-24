6f00a3a47303dc56270a4e684b782111a1bd32d2
Steve Zhu <704242527zzh@gmail.com>
2018-11-24 17:30:32 -0500

    new commit

--- python/main.py
+++ python/main.py
@@ -1,17 +1,12 @@
 import numpy as np
 import nn
+import q4
+from skimage import io
 
-acc_plot = np.arange(50)
-valid_acc_plot = np.arange(50)*2
-time_seq = np.arange(50)
-import matplotlib.pyplot as plt
+image=io.imread('../images/01_list.jpg')
+# io.imshow(image)
+# io.show()
+# print(type(image))
 
-plt.figure(1)
-ax = plt.gca()
-ax.set_xlabel('epoch')
-ax.set_ylabel('loss')
 
-ax.plot(time_seq, acc_plot, color='r', linewidth=1, alpha=0.6)
-ax.plot(time_seq, valid_acc_plot, color='b', linewidth=1, alpha=0.6)
-plt.pause(1500)
-plt.close()
+q4.findLetters(image)
